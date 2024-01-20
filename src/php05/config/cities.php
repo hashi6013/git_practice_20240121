6af6b254348530fa66ce23dae6f0f0cd53316748
hashimoto takumi <takumih6013@gmail.com>
2024-01-21 06:46:23 +0900

    コミットする

---
+++ src/php05/config/cities.php
@@ -0,0 +1,44 @@
+<!-- 国の情報を入れるファイル -->
+
+<?php
+$cities = [
+    [
+        'name' => 'シドニー',
+        'time_zone' => 'Australia/Sydney',
+        'img' => 'australia.jpg'
+    ],
+    [
+        'name' => '東京',
+        'time_zone' => 'Asia/Tokyo',
+        'img' => 'japan.jpg'
+    ],
+    [
+        'name' => '上海',
+        'time_zone' => 'Asia/Shanghai',
+        'img' => 'china.jpg'
+    ],
+    [
+        'name' => 'モスクワ',
+        'time_zone' => 'Europe/Moscow',
+        'img' => 'russia.jpg'
+    ],
+    [
+        'name' => 'ロンドン',
+        'time_zone' => 'Europe/London',
+        'img' => 'british.jpg'
+    ],
+    [
+        'name' => 'ヨハネスブルグ',
+        'time_zone' => 'Africa/Johannesburg',
+        'img' => 'south-africa.jpg'
+    ],
+    [
+        'name' => 'ニューヨーク',
+        'time_zone' => 'America/New_York',
+        'img' => 'america.jpg'
+    ]
+];
+// 変数citiesに配列（シドニー～ニューヨーク）を入れろ
+
+
+
